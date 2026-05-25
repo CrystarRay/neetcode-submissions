d538bf72a14c2efaa2dcdc45ba765b3e89e6b7d9
Chenrui <126846127+CrystarRay@users.noreply.github.com>
2026-05-25 09:04:12 -0700

    Bulk sync: 16 submissions

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        
+        counter = 0
+        # details[0][10:12]
+        for i in details:
+            if int(i[11:13]) > 60:
+                counter += 1
+        
+        return counter
